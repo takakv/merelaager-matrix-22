277364a10609bfc0920a7ed39430d32f48a59d3e
Taka <71265675+takakv@users.noreply.github.com>
2022-08-03 15:30:17 +0300

    Adjust canvas scroll

--- public/media/scripts/matrix.js
+++ public/media/scripts/matrix.js
@@ -1,51 +1,67 @@
+(function () {
+
 // geting canvas by Boujjou Achraf
-var c = document.getElementById("c");
-var ctx = c.getContext("2d");
+  var c = document.getElementById("c");
+  var ctx = c.getContext("2d");
 
-//making the canvas full screen
-c.height = window.innerHeight;
-c.width = window.innerWidth;
+// https://stackoverflow.com/questions/4288253/html5-canvas-100-width-height-of-viewport
+  window.addEventListener('resize', resizeCanvas, false);
 
-//chinese characters - taken from the unicode charset
-var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
+  function resizeCanvas() {
+    c.width = window.innerWidth;
+    c.height = document.querySelector("body").clientHeight;//window.innerHeight;
+
+    /**
+     * Your drawings need to be inside this function otherwise they will be reset when
+     * you resize the browser window and the canvas goes will be cleared.
+     */
+    //draw();
+    setInterval(draw, 35);
+  }
+
+  resizeCanvas();
+
+  //chinese characters - taken from the unicode charset
+  var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
 //converting the string into an array of single characters
-matrix = matrix.split("");
+  matrix = matrix.split("");
 
-var font_size = 10;
-var columns = c.width/font_size; //number of columns for the rain
+  var font_size = 10;
+  var columns = c.width / font_size; //number of columns for the rain
 //an array of drops - one per column
-var drops = [];
+  var drops = [];
 //x below is the x coordinate
 //1 = y co-ordinate of the drop(same for every drop initially)
-for(var x = 0; x < columns; x++)
-  drops[x] = 1;
+  for (var x = 0; x < columns; x++)
+    drops[x] = 1;
 
 //drawing the characters
-function draw()
-{
-  //Black BG for the canvas
-  //translucent BG to show trail
-  ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
-  ctx.fillRect(0, 0, c.width, c.height);
+  function draw() {
+    //Black BG for the canvas
+    //translucent BG to show trail
+    ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
+    ctx.fillRect(0, 0, c.width, c.height);
 
-  ctx.fillStyle = "#94f475";//green text
-  ctx.font = font_size + "px arial";
-  //looping over drops
-  for(var i = 0; i < drops.length; i++)
-  {
-    //a random chinese character to print
-    var text = matrix[Math.floor(Math.random()*matrix.length)];
-    //x = i*font_size, y = value of drops[i]*font_size
-    ctx.fillText(text, i*font_size, drops[i]*font_size);
+    ctx.fillStyle = "#94f475";//green text
+    ctx.font = font_size + "px arial";
+    //looping over drops
+    for (var i = 0; i < drops.length; i++) {
+      //a random chinese character to print
+      var text = matrix[Math.floor(Math.random() * matrix.length)];
+      //x = i*font_size, y = value of drops[i]*font_size
+      ctx.fillText(text, i * font_size, drops[i] * font_size);
 
-    //sending the drop back to the top randomly after it has crossed the screen
-    //adding a randomness to the reset to make the drops scattered on the Y axis
-    if(drops[i]*font_size > c.height && Math.random() > 0.975)
-      drops[i] = 0;
+      //sending the drop back to the top randomly after it has crossed the screen
+      //adding a randomness to the reset to make the drops scattered on the Y axis
+      if (drops[i] * font_size > c.height && Math.random() > 0.975)
+        drops[i] = 0;
 
-    //incrementing Y coordinate
-    drops[i]++;
+      //incrementing Y coordinate
+      drops[i]++;
+    }
   }
-}
+})();
 
-setInterval(draw, 35);
+// setInterval(draw, 35);
+
+console.log(document.querySelector("body").clientHeight);
